286c2f1367a29719ef31c5c244e7b9886c6e8154
Daniel <danielptm@me.com>
2018-07-17 19:09:24 +0200

    bugfix

--- start.sh
+++ start.sh
@@ -3,9 +3,11 @@
 echo '#####';
 echo 'When the build process is complete, see the app on localhost:8081';
 echo '#####';
-npm install;
+
 cd tuttleFrontEnd;
+npm install;
 npm run build;
 cd ../node-seed;
+npm install;
 npm run start;
 
